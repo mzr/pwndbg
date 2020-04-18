ed5bb69bda0b6caa27cc13b339a7b9eb7d7e5a7a
Jan Mazur <mzr4141@gmail.com>
2020-04-18 17:52:26 +0200

    trails prototype2

--- pwndbg/commands/dumptrail.py
+++ pwndbg/commands/dumptrail.py
@@ -18,19 +18,31 @@ import pwndbg.wrappers.readelf
 from pwndbg.color import message
 
 parser = argparse.ArgumentParser(description='Show the state of the Global Offset Table')
-parser.add_argument('name_filter', help='Filter results by passed name.',
+parser.add_argument('filename', help='Filter results by passed name.',
                     type=str, nargs='?', default='')
 
 from pwndbg.next import jumps
 
+trails = {}
+
 def dump_from_to(start, end, filename=None):
     iter_addr = start
+    log = ""
     while iter_addr <= end:
         ins = pwndbg.disasm.one(iter_addr)
         # TUTAJ DUMPOWANIE
-        print('{}: {}'.format(hex(ins.address), ins.insn_name()))
+        #print('{}: {}'.format(hex(ins.address), ins.insn_name()))
+        log += "0x%x:\t%s\t%s\n" %(ins.address, ins.mnemonic, ins.op_str)
+
         # TUTAJ DUMPOWANIE
         iter_addr = pwndbg.disasm.one(ins.next).address
+
+    if filename:
+        with open(filename, 'a') as f:
+            f.write(log)
+            f.write('\n')
+    else:
+        print(log)
 
 def better_next_branch():
     ins = pwndbg.disasm.one(pwndbg.regs.pc)
@@ -40,17 +52,20 @@ def better_next_branch():
 
 @pwndbg.commands.ArgparsedCommand(parser)
 @pwndbg.commands.OnlyWhenRunning
-def dumptrail(name_filter=''):
+def dumptrail(filename=''):
     current_addr = pwndbg.regs.rip
 
     nextjump_addr = better_next_branch().address
-    dump_from_to(current_addr, nextjump_addr)
+    dump_from_to(current_addr, nextjump_addr, filename=filename)
     return nextjump_addr
 
 @pwndbg.commands.ArgparsedCommand(parser)
 @pwndbg.commands.OnlyWhenRunning
-def conttrail(name_filter=''):
-    dumptrail()
+def conttrail(filename=''):
+    current_addr = pwndbg.regs.rip
+    if current_addr not in trails:
+        dumptrail(filename=filename)
+        trails[current_addr] = 1
     pwndbg.next.break_next_branch()
     gdb.execute('stepi')
 
